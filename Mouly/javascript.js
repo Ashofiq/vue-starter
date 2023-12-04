c2e098439f7130c52f26ecfd992ae1043cb9d3f1
israth697 <israth.jarin.23@gmail.com>
2023-12-04 18:41:03 +0600

    practice

---
+++ Mouly/javascript.js
@@ -0,0 +1,5 @@
+var java="language"
+
+ function addNumbers(a, b) {
+        return a + b; ;
+    }
---
+++ Mouly/loop.js
@@ -0,0 +1,33 @@
+//forloop
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i + ": " + i*3 + "<br />");
+//     }
+//     var sum = 0;
+//     var a=[1,2,3,5];
+//     for (var i = 0; i < a.length; i++) {
+//     sum +=a[i];
+//     }               // parsing an array
+//     html = "";
+
+
+//while loop
+// var i = 1;                      // initialize
+// while (i < 100) {               // enters the cycle if statement is true
+// i *= 2;                     // increment to avoid infinite loop
+// document.write(i + ", ");   // output
+// }
+
+
+//do while loop
+// var i = 1;                      // initialize
+// do {                            // enters cycle at least once
+// i *= 5;                     // increment to avoid infinite loop
+// document.write(i + ", ");   // output
+// } while (i < 100)
+
+
+var a=['moneyplant','top','stone','roses','fruits'];
+console.log(a[1]);
+    
+
